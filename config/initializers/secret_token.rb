acb83bfdf5c893ed38a0b194b927c440c1852bcb
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-03-03 21:40:59 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheOtherSongClinic::Application.config.secret_token = '27083a4ddcdff88e2d4a77c3c3c642ff9ca97fca02f13d312168afc90c0ac72ca626cc3438449839b94631c2df9a3fb708dc9d0a009bdb8d36c7b1d424b11fb9'
