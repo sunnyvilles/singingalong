9251770f12d1e0765dbe868cf6f24186ea09241a
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-03-04 00:12:22 +0530

    user registration stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+	def register
+		
+	end
+
+	def register_user
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ TheOtherSongClinic::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 	match 'article/:id' => 'articles#show'
+
+	match '/register' => 'home#register', :via => 'get'
+	match '/register' => 'home#register_user', :via => 'post'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
