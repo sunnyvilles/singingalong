8d6cb2d903a415de04c87ee032ea611b5d6a7d2b
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-03-30 20:48:45 +0530

    added admin namespace

---
+++ app/controllers/web/activity_controller.rb
@@ -0,0 +1,3 @@
+class Web::ArticlesController < ApplicationController
+	
+end
---
+++ app/controllers/web/articles_controller.rb
@@ -0,0 +1,14 @@
+class Web::ArticlesController < ApplicationController
+
+	# GET /articles/1
+  # GET /articles/1.json
+  def show
+    @article = Article.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json=> @article }
+    end
+  end
+
+end
