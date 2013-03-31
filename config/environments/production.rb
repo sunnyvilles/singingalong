659d2a667963feb0e5acf8d551b1c5793f708a14
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-03-31 13:35:01 +0530

    vendor compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ TheOtherSongClinic::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+	config.assets.precompile += %w( public.js public.css  )
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
