860042f444d45d6d71142b1734141a7072d30435
sunnyera <sunny.era@gmail.com>
2013-07-09 18:50:45 +0530

    bad app_cont.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
 #before_filter :prepare_for_mobile
 
+before_filter :set_locale
+def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+end
+
 protect_from_forgery
 
 
