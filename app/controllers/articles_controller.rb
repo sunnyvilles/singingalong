b6fa5bf105b6f3f16ef9dde792d93e66cfb76f0f
sunnyera <sunny.era@gmail.com>
2013-07-24 00:28:16 +0530

    articles backend non-admin

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+
+  	@articles = Article.all
+    respond_to do |format|
+      format.html 
+      format.json { render json: @articles }
+    end
+  end
+
+  def show
+@article = Article.find(params[:id])
+
+    respond_to do |format|
+      format.html 
+      format.json { render json: @article }
+    end
+
+  end
+end
