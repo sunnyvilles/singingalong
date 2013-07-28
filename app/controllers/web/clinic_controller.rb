7773cc8ff31e5d92fcf2d851eca294fcc9a364f1
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-07-28 19:28:06 +0530

    adding gemfile

--- app/controllers/web/clinic_controller.rb
+++ app/controllers/web/clinic_controller.rb
@@ -15,18 +15,29 @@ class Web::ClinicController < ApplicationController
 	def article
 
     @article = Article.find(params[:article_id])
-	puts "aaaaaaaaaaaaaaaa share count " + @article.viewcount.to_s
+		puts "aaaaaaaaaaaaaaaa share count " + @article.viewcount.to_s
 
     @article.viewcount += 1
 
     @article.save
 
-  rescue => e
-  logger.error( 'couldnt increase share count ' + e.to_s )
-  flash[:error] = 'couldnt increase share count'
- 
-  	end
+		rescue => e
+			logger.error( 'couldnt increase share count ' + e.to_s )
+			flash[:error] = 'couldnt increase share count' 
+	end
 
+	def case_studies
+  	per_page = 10
+  	page = params[:page].to_i == 0 ? 1 : params[:page].to_i
+  	start_value = (page - 1 )*per_page
+
+
+    @case_studies = Article.find(:all, :conditions => [ "source = ? and type=?", "clinic","case study" ], :order => 'created_at DESC', :offset => start_value, :limit => per_page)
+
+    @total_case_studies = Article.find(:all, :conditions => [ "source = ? and type=?", "clinic", "case study" ])
+    @total_case_studies_remain = @total_case_studies.size - start_value
+
+  end
 	def about
 		
 	end
