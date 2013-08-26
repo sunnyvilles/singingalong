a00d3bd18383ad5e1e6da97d6072198ea35115d7
sunnyera <sunny.era@gmail.com>
2013-08-27 00:10:00 +0530

    login routes

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,8 +2,12 @@ class Admin::AdminController < ApplicationController
 	before_filter :confirm_logged_in, :except => [:login, :attempt_login]
 	layout "admin/application"
 	def show
-		if(!is_logged_in?)
-			render "/admin/shared/login"
+		if is_logged_in?
+			if (!is_admin?)
+				redirect_to "/clinic"
+			end			
+		else
+			redirect_to "/clinic"
 		end
 	end
 end
