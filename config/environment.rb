e3b4cf72ea5c0e2af6eb4fb08bba9d0bc0103d27
abhimanyusaxena <abhimanyusaxena@gmail.com>
2014-01-10 00:58:07 +0530

    try

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+	:address=>              'smtpauth.theothersong.com',
+  :port=>                 587,
+	:domain=>               'theothersong.com',
+	:user_name=>             'webadmin@theothersong.com',
+	:password=>               'Vital123$',
+  :authentication=>       'plain',
+  :enable_starttls_auto=> true
+}
 # Initialize the rails application
 TheOtherSongClinic::Application.initialize!
